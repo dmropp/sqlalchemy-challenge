1f3560c3af793afa44f0d07e624813aec0508279
dmropp <134107540+dmropp@users.noreply.github.com>
2023-09-01 13:55:49 -0700

    2023.09.01 13:55 commit

--- HawaiianVacation/app.py
+++ HawaiianVacation/app.py
@@ -1,33 +1,54 @@
 # Import the dependencies.
 
-from flask import Flask
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
+# engine = create_engine("sqlite:///Resources//hawaii.sqlite")
+
+# https://stackoverflow.com/questions/4636970/sqlite3-operationalerror-unable-to-open-database-file, referenced for how to fix unable to open database file error
+engine = create_engine("sqlite:///C:\\Users\\dmrop\\Desktop\\Assignments\\Starter_Code\\sqlalchemy-challenge\\HawaiianVacation\\Resources\\hawaii.sqlite")
+
+
 
 # reflect an existing database into a new model
 
-
+Base = automap_base()
 
 # reflect the tables
 
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
 
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
 
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
